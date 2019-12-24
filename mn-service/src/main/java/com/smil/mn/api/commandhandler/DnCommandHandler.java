8130edc66da7d4af6c1a10051d3c2a5784f251ea
wangwei <1980871239@qq.com>
2019-12-24 09:51:51 +0800

    test add

---
+++ mn-service/src/main/java/com/smil/mn/api/commandhandler/DnCommandHandler.java
@@ -0,0 +1,4 @@
+package com.smil.mn.api.commandhandler;
+
+public interface DnCommandHandler {
+}
---
+++ mn-service/src/main/java/com/smil/mn/api/commandhandler/impl/DnCommandHandlerImpl.java
@@ -0,0 +1,6 @@
+package com.smil.mn.api.commandhandler.impl;
+
+import com.smil.mn.api.commandhandler.DnCommandHandler;
+
+public class DnCommandHandlerImpl implements DnCommandHandler {
+}
